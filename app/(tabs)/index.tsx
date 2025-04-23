b36ab6493b5305102055038cfc4ccecc16345eb6
DeftSlyNinja <christopher_masson@hotmail.com>
2025-04-23 16:24:48 -0700

    Organized Views

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,46 @@
 import { Image, StyleSheet, Platform, ScrollView, FlatList, TextInput, Button } from 'react-native';
-
+import { Colors } from '@/constants/Colors';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
+/* 
+    Assignment Requirements:
+    - Text Component
+    - TextInput Component
+    - Button (Optional)
+    - ScrollView Component
+    - FlatList Component
+    - Style w/ Flexbox
+    - Optimize for both small and large screens
+*/
+
 export default function HomeScreen() {
   return (
-    <ScrollView style={styles.container}>
-
-      <ThemedText type="subtitle">
-        My Game Library
-      </ThemedText>
-
-      <TextInput style={styles.searchBar} placeholder='Enter game title...'></TextInput>
-      <Button title="Submit"></Button>
+    <ThemedView style={styles.container}>
       <ThemedView style={styles.titleContainer}>
-
-        <FlatList 
-          data={videoGames} 
-          keyExtractor={(item) => item.id} 
-          renderItem={({ item }) => (
-            <ThemedView style={styles.stepContainer}>
-              <ThemedText type="title">{item.title}</ThemedText>
-            </ThemedView>
-          )}>
-          
-        </FlatList>
-
+        <ThemedText type="title">
+          My Game Library
+        </ThemedText>
       </ThemedView>
-    </ScrollView>
+      <ThemedView style={styles.inputContainer}>
+        <TextInput style={styles.searchBar} placeholder='Search for game title...'></TextInput>
+
+        <Button title="Submit"></Button>
+      </ThemedView>
+      <ScrollView style={styles.scrollContainer}>
+        <ThemedView style={styles.listContainer}>
+          <FlatList 
+            data={videoGames} 
+            keyExtractor={(item) => item.id} 
+            renderItem={({ item }) => (
+              <ThemedView>
+                <ThemedText type="title">{item.title}</ThemedText>
+              </ThemedView>
+            )}>
+          </FlatList>
+        </ThemedView>
+      </ScrollView>
+    </ThemedView>
   );
 }
 
@@ -44,6 +57,16 @@ const videoGames = [
   { id: '8', title: 'Skyrim' },
   { id: '9', title: 'Dota 2' },
   { id: '10', title: 'League of Legends' },
+  { id: '11', title: 'Diablo' },
+  { id: '12', title: 'GTA' },
+  { id: '13', title: 'The Last Spell' },
+  { id: '14', title: 'Brotatoe' },
+  { id: '15', title: 'Dungeons of Dredmor' },
+  { id: '16', title: 'Apex Legends' },
+  { id: '17', title: 'Final Fantasy XIV' },
+  { id: '18', title: 'World of Warcraft' },
+  { id: '19', title: 'Slay the Spire' },
+  { id: '20', title: 'Balatro' },
 ];
 
 // Styles below
@@ -56,10 +79,19 @@ const styles = StyleSheet.create({
     margin: 10
   },
   container: {
+
+    // borderColor: 'red',
+    // borderWidth:5,
+
     flex: 1,
     padding: 16,
   },
-  titleContainer: {
+  listContainer: {
+
+    // borderColor: 'red',
+    // borderWidth:5,
+
+    flex: 1,
     borderRadius: 5,
     margin: 10,
     padding: 10,
@@ -67,15 +99,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  titleContainer: {
+
+    // borderColor: 'red',
+    // borderWidth:5,
+
+    // flex:1,
+    alignItems: 'center'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  inputContainer: {
+
+    // borderColor: 'red',
+    // borderWidth:5,
+
+    // flex:1,
   },
+  scrollContainer: {
+    // borderColor: 'red',
+    // borderWidth:5,
+
+    flex:4,
+  }
 });
